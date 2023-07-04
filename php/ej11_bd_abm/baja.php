4bd30ab0a4c909d34e69e37bfc560ed63b0475f8
ramatc <rtanquiascornejo@gmail.com>
2023-07-04 14:09:42 -0300

    ABM casi finalizada

--- php/ej11_bd_abm/baja.php
+++ php/ej11_bd_abm/baja.php
@@ -1,3 +1,33 @@
 <?php
 
+$dbname = "boter0xpekexi1ebyzxp";
+$host = "boter0xpekexi1ebyzxp-mysql.services.clever-cloud.com";
+$user = "uf7hf1x9lseshxyt";
+$password = "lG2kzZOSuVCwrD0sIntd";
+$respuesta_estado = "";
+
+try {
+    $dsn = "mysql:host=$host;dbname=$dbname";
+    $dbh = new PDO($dsn, $user, $password);
+    $respuesta_estado = $respuesta_estado . "\nConexión exitosa.";
+
+    $codArt = $_POST["codArt"];
+
+    $sql = "DELETE FROM zapatillas WHERE codArt = :codArt";
+
+    $stmt = $dbh->prepare($sql);
+    $respuesta_estado = $respuesta_estado . "\nPreparación exitosa.";
+
+    $stmt->bindParam(":codArt", $codArt);
+    $respuesta_estado = $respuesta_estado . "\nBind exitoso.";
+
+    $stmt->execute();
+    $respuesta_estado = $respuesta_estado . "\nEjecución exitosa.";
+
+    echo $respuesta_estado;
+} catch (PDOException $e) {
+    $respuesta_estado = $respuesta_estado . "\nERROR: \n" . $e->getMessage();
+    echo $respuesta_estado;
+}
+
 ?>
---
+++ php/ej11_bd_abm/pdf.php
@@ -0,0 +1,33 @@
+<?php
+
+$dbname = "boter0xpekexi1ebyzxp";
+$host = "boter0xpekexi1ebyzxp-mysql.services.clever-cloud.com";
+$user = "uf7hf1x9lseshxyt";
+$password = "lG2kzZOSuVCwrD0sIntd";
+
+try {
+    $dsn = "mysql:host=$host;dbname=$dbname";
+    $dbh = new PDO($dsn, $user, $password);
+
+    $codArt = $_GET["codArt"];
+
+    $sql = "SELECT archivo FROM zapatillas WHERE codArt = :codArt";
+
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindParam(':codArt', $codArt);
+
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $stmt->execute();
+
+    $fila = $stmt->fetch();
+    $objZapatilla = new stdClass;
+    $objZapatilla->documentoPDF = base64_encode($fila["archivo"]);
+    $salidaJSON = json_encode($objZapatilla, JSON_INVALID_UTF8_SUBSTITUTE);
+    echo $salidaJSON;
+    $dbh = null;
+
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+?>
