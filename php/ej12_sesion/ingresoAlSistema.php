5e951d9394a829ad4078130f62467804d267e4df
ramatc <rtanquiascornejo@gmail.com>
2023-07-06 16:51:30 -0300

    Prueba

--- php/ej12_sesion/ingresoAlSistema.php
+++ php/ej12_sesion/ingresoAlSistema.php
@@ -1,61 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ingreso al sistema</title>
-    <style>
-        * {
-            box-sizing: border-box;
-            padding: 0;
-            margin: 0;
-        }
-
-        html,
-        body {
-            width: 100%;
-            height: 100%;
-            font-family: sans-serif;
-            color: white;
-        }
-
-        body {
-            background: linear-gradient(#243b55, #141e30);
-        }
-
-        h1, h2, h3 {
-            margin: 15px;
-        }
-        
-        div {
-            padding: 10px;
-            margin: 15px;
-            width: 40%;
-            border: 2px solid black;
-            border-radius: 5px;
-        }
-
-        button {
-            background-color: #fafafa;
-            color: #252525;
-            cursor: pointer;
-            font-size: 18px;
-            padding: 7px;
-            border: none;
-            border-radius: 4px;
-            transition: all 0.6s ease;
-            margin: 15px;
-        }
-
-        button:hover {
-            background-color: blue;
-            color: white;
-        }
-    </style>
-</head>
-
-<body>
 <?php
 
 include('./conexionBd.inc');
@@ -94,17 +36,14 @@ if (!isset($_SESSION['idSesion'])) {
 
 echo "<h1>Accesso permitido</h1>";
 echo "<h2>Sus parametros de sesión son los siguientes: </h2>";
-echo "<div>
+echo "<div style='border: 1px solid black'>
     <h2>Información de sesión</h2>
-    <h3>ID de sesión: <span style='color: #04ffea'>" . $_SESSION['idSesion'] . "</span></h3>
-    <h3>Login de usuario: <span style='color: #04ffea'>" . $_SESSION['login'] . "</span></h3>
-    <h3>Contador de sesión: <span style='color: #04ffea'>" . $contadorBd . "</span></h3>
+    <h3>ID de sesión: <span style='color: blue'>" . $_SESSION['idSesion'] . "</span></h3>
+    <h3>Login de usuario: <span style='color: blue'>" . $_SESSION['login'] . "</span></h3>
+    <h3>Contador de sesión: <span style='color: blue'>" . $contadorBd . "</span></h3>
     </div>";
 
-echo "<button onClick=\"location.href='./app_modulo1'\">Ingresar a la aplicación</button>";
+echo "<button onClick=\"location.href='./app'\">Ingresar a la aplicación</button>";
 echo "<button onClick=\"location.href='./destruirSesion.php'\">Terminar sesión</button>";
 
 ?>
-</body>
-
-</html>
